e3287c6ef5acc32e88a7ac214d63a03c1bb1c944
anhol <holo.linuxlv@gmail.com>
2026-09-11 16:43:11 -0400

    fix (file mode): Added strict 0400 mode policy for auth file

--- src/vauth.cpp
+++ src/vauth.cpp
@@ -253,11 +253,9 @@ void read_authorization(
             "service user"
         );
     }
-    if((status.st_mode & (S_IWGRP | S_IWOTH)) != 0) {
-        throw std::runtime_error(
-            "Database authorization credential must not be writable by "
-            "group or others"
-        );
+    if ((status.st_mode & 0700) != 0400) {
+        throw std::runtime_error("Database authorization credential must not "
+                                 "be writable by anybody");
     }
     if(status.st_size < 1 || status.st_size > 34) {
         throw std::runtime_error(
